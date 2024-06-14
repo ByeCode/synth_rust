7ecb39b91e710e02c16afcb0def4668644ee6f15
amv2 <amv2@hw.ac.uk>
2024-06-15 03:04:03 +0400

    init backend

---
+++ synth_backend/target/debug/deps/synth_backend-caeb4af01abffce1.d
@@ -0,0 +1,5 @@
+/Users/moses/Codespace/test/rust/synth_rust/synth_backend/target/debug/deps/libsynth_backend-caeb4af01abffce1.rmeta: src/main.rs
+
+/Users/moses/Codespace/test/rust/synth_rust/synth_backend/target/debug/deps/synth_backend-caeb4af01abffce1.d: src/main.rs
+
+src/main.rs:
